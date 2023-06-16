faaa90e80b1af53047181f6f25e496f120a17480
I494715 <i494715@fhict.nl>
2023-06-16 05:57:14 +0200

    Minor fixes

---
+++ sneakpeak/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYDkEOlAgTf_1cXUWAG5G7LP5PRVFqmQM",
+  authDomain: "sneakpeak-app.firebaseapp.com",
+  projectId: "sneakpeak-app",
+  storageBucket: "sneakpeak-app.appspot.com",
+  messagingSenderId: "117509565432",
+  appId: "1:117509565432:web:dda7436c7266e12413440d",
+  measurementId: "G-5VRVD6569V",
+};
+
+export default firebaseConfig;
